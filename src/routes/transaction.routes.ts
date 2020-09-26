ece45c22701b8547b061bf3e3af219a9a0b9a96c
jadsom bruno <jadsombruno2016@gmail.com>
2020-09-26 12:17:18 -0300

    Allow create a transaction

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -30,7 +30,13 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    const {title, value, type} = request.body;
+
+    const createTransaction = new CreateTransactionService(transactionsRepository);
+
+    const transaction = createTransaction.execute({title, value, type});
+
+    return response.status(200).json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
