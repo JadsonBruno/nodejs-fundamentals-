ff862deb3c746bdf808c685a745abe735381dd9b
jadsom bruno <jadsombruno2016@gmail.com>
2020-09-26 12:15:14 -0300

    Create Transaction Service

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,18 @@
+/**
+ * IMPORTS
+ */
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+/**
+ * TYPES
+ */
+import {Request} from './index.d';
+
+
+/**
+ * CODE
+ */
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,9 +20,26 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({title, type, value}: Request): Transaction {
+    const {total} = this.transactionsRepository.getBalance();
+
+    // outcome transaction and total is less than the desired amount: throw error
+    if (type === 'outcome' && value > total)
+    {
+      throw Error('Insufficient balance to complete the transaction');
+    }
+
+    // create transaction
+    const transaction = this.transactionsRepository
+                            .create({title, value, type});
+
+    // return created transaction                            
+    return transaction;
   }
 }
 
+
+/**
+ * EXPORTS
+ */
 export default CreateTransactionService;
